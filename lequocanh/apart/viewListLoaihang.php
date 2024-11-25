1b6950251cb8eec1679ca3646a4c9488ad58050f
lequocanh-01 <quocanh5758@gmail.com>
2024-11-25 11:33:21 +0700

    hoan thanh

--- lequocanh/apart/viewListLoaihang.php
+++ lequocanh/apart/viewListLoaihang.php
@@ -12,6 +12,32 @@ if (isset($_GET['reqView'])) {
 }
 ?>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+<div id="carouselExample" class="carousel slide mb-4" data-bs-ride="carousel" data-bs-interval="3000">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="./administrator/img_LQA/Xiaomi-12T.png" class="d-block w-100" alt="Slide 1">
+        </div>
+        <div class="carousel-item">
+            <img src="./administrator/img_LQA/iphone 13.png" class="d-block w-100" alt="Slide 2">
+        </div>
+        <div class="carousel-item">
+            <img src="./administrator/img_LQA/galaxy s21.png" class="d-block w-100" alt="Slide 3">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
 <div class="row row-cols-1 row-cols-md-3 g-4">
     <?php
     foreach ($list_hanghoa as $v) {
